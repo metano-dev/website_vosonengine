2552e63405819b8b70297aa4a662c689b9ad9956
FaustoSantarosa <144014829+FaustoSantarosa@users.noreply.github.com>
2024-05-15 00:53:28 +0200

    Update eventos.js

--- script/eventos.js
+++ script/eventos.js
@@ -253,6 +253,7 @@ function popup (dir) {
 			videoM.height   = fondo.height;
 			fondo.appendChild(videoM);
 		}
+		console.log(dir.split("video")[0]);
 		fondo.style.backgroundImage = "url("+dir.split("video")[0] + "espera.jpg"+")";
 		//fondo.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/f/f2/Philip_I_of_Taranto_statue.png')";
 		//fondo.style.backgroundColor = "#F00";
